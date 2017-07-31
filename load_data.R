878c564b0fff7dec7b9a866bd54406501762d295
luca valnegri <l.valnegri@datamaps.co.uk>
2017-07-31 12:52:59 +0100

    started to change way to retrieve files

--- load_data.R
+++ load_data.R
@@ -1,30 +1,30 @@
 # source data main page: http://ratings.food.gov.uk/open-data
-# example URL for LAD: http://ratings.food.gov.uk/OpenDataFiles/FHRS528en-GB.xml
+# example URL for LAD XXX: http://ratings.food.gov.uk/OpenDataFiles/FHRSXXXen-GB.xml
 
-lapply(c('data.table', 'RMySQL', 'xml2'), require, character.only = TRUE)
+lapply(c('data.table', 'RMySQL', 'rvest', 'xml2'), require, character.only = TRUE)
 
-dbc = dbConnect(MySQL(), group = 'homeserver', dbname = 'food_hygiene_ratings')
+dbc = dbConnect(MySQL(), group = 'dataOps', dbname = 'uk_food_hygiene_ratings')
 
-# print(paste('Processing scores descriptions...'))
-# scores <- unlist(t(xmlToList(xmlTreeParse('http://ratings.food.gov.uk/open-data-resources/lookupData/ScoreDescriptors.xml'))[[3]]))
-# scores <- scores[(length(scores)/6*2+1):length(scores)]
-# scores <- as.data.frame(matrix(scores, ncol = 4, byrow = FALSE)) 
-# names(scores) <- c('description', 'id', 'value', 'category')
-# dbWriteTable(db_conn, 'scores', scores, rownames = FALSE, append = TRUE)
-# print(paste('DONE!'))
+lads <- read_html('http://ratings.food.gov.uk/open-data') %>%
+            html_nodes('#openDataStatic a') %>%
+            html_attr('href') %>%
+            as.data.frame() %>%
+            sort()
+names(lads) <- 'lad_id'
+lads <- subset(lads, !grepl('cy', lad_id)) 
+lads$lad_id <- substr(lads$lad_id, 46, 48)
 
 
 bizTypeIDs <- data.frame(V0 = character(0), V1 = numeric(0))
 geoCoord <- data.frame(V1 = character(0), V2 = numeric(0), V3 = numeric(0))
 
 print(paste('Processing local authorities'))
-LAD <- dbReadTable(db_conn, 'LAD')
-for(idx in 1:nrow(LAD)){
-    print(paste('Processing file n. ', idx,' out of ',nrow(LAD), '. Local authority: ', LAD[idx, 2], ' - ', LAD[idx, 3], '.', sep = '') )
-    xml.url <- paste('http://ratings.food.gov.uk/OpenDataFiles/FHRS', ifelse(LAD[idx, 2] < 100, '0', ''), LAD[idx, 2], 'en-GB.xml', sep = '')
+for(idx in 1:nrow(lads)){
+    print(paste('Processing file n. ', idx,' out of ',nrow(lads) ) )
+    xml.url <- paste0('http://ratings.food.gov.uk/OpenDataFiles/FHRS', lads[idx], 'en-GB.xml')
     xmlfile <- xmlToList(xmlTreeParse(xml.url))[[2]]
     print(paste('Updating count of shops:', length(xmlfile)))
-    dbSendQuery(db_conn, paste('UPDATE LAD SET shops =', length(xmlfile), 'WHERE FLA_ID =', LAD[idx, 2]) )
+    dbSendQuery(db_conn, paste('UPDATE lads SET shops =', length(xmlfile), 'WHERE fla_id =', lads[idx]) )
 
     geoCoord <- rbind(geoCoord,
                         as.data.frame(cbind(
